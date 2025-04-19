5c41834f14003f6b690e48840dea154d39e4d8c2
bcbears <bouldercitybearsfb@gmail.com>
2025-04-18 22:03:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -173,6 +173,8 @@ function showQuestion() {
 }
 
 function showResults() {
+  if (resultSound) resultSound.play();
+
   const counts = { A: 0, B: 0, C: 0, D: 0, E: 0 };
   answers.forEach(a => counts[a]++);
 
